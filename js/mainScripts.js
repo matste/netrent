45ddb58acfbb61921abf062e1ec80c68acca72c6
Stephan Mattalo <stephanmattalo@gmail.com>
2018-02-19 21:48:58 -0500

    Added sliding on menu and added chat window bottom right

---
+++ js/mainScripts.js
@@ -0,0 +1,15 @@
+function openNav() {
+  document.getElementById("sideNav").style.width = "200px";
+}
+
+function closeNav() {
+  document.getElementById("sideNav").style.width = "0px";
+}
+
+function openChat() {
+  document.getElementById("chatBar").style.height = "300px";
+}
+
+function closeChat() {
+  document.getElementById("chatBar").style.height = "40px";
+}
